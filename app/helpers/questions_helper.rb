87766778af37c383d944a97a98344cb303b16bb2
Tatiana Morozova <tabobo@mail.ru>
2019-12-16 16:46:56 +0300

    helpers

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 module QuestionsHelper
-
-  
+  def question_header(_test)
+    controller.action_name.capitalize + ' ' + @test.title + ' question'
+  end
 end
--- app/helpers/tests_helper.rb
+++ app/helpers/tests_helper.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 module TestsHelper
-
   TEST_LEVELS = { 0 => :easy, 1 => :elementary, 2 => :advanced, 3 => :hard }.freeze
 
   def test_level(test)
@@ -20,5 +19,4 @@ module TestsHelper
     #   'hero'
     # end
   end
-
 end
