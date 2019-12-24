fb0dd4741a4322d51f82993a871aebe2aab96ea0
Tatiana Morozova <tabobo@mail.ru>
2019-12-24 14:40:45 +0300

    delete before_action

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,29 +1,27 @@
 # frozen_string_literal: true
 
 class QuestionsController < ApplicationController
-  before_action :set_question, only: %i[show edit update destroy]
-  before_action :get_test, except: %i[new create]
+  #before_action :set_question, only: %i[show edit update destroy]
+  #before_action :get_test, except: %i[new create]
   # rescue_from ActiveRecord::RecordNotFound, with: :render_404
 
-  # GET /questions
   def index
     @questions = @test.questions
   end
 
-  # GET /questions/1
   def show
+    @question = Question.find(params[:id])
   end
 
-  # GET /questions/new
   def new
     @test = Test.find(params[:test_id])
     @question = @test.questions.build
   end
 
-  # GET /quetions/1/edit
-  def edit; end
+  def edit
+    @question = Question.find(params[:id])
+  end
 
-  # POST /questions
   def create
     @test = Test.find(params[:test_id])
     @question = @test.questions.new(question_params)
@@ -39,8 +37,8 @@ class QuestionsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /questions/1
   def update
+    @question = Question.find(params[:id])
     respond_to do |format|
       if @question.update(question_params)
         format.html { redirect_to test_path(@test, @question), notice: 'Question was successfully updated.' }
@@ -58,8 +56,8 @@ class QuestionsController < ApplicationController
     render plain: result.join("\n")
   end
 
-  # DELETE /questions/1
   def destroy
+    @question = Question.find(params[:id])
     @question.destroy
     respond_to do |format|
       format.html { redirect_to test_path(@test), notice: 'Question was successfully destroyed.' }
@@ -69,18 +67,6 @@ class QuestionsController < ApplicationController
 
   private
 
-  # before
-  # http://localhost:3000/tests/1/questions/1
-  # def get_test
-  #   @test = Test.find(params[:test_id])
-  # end
-  # def set_question
-  #   @question = @test.questions.find(params[:id])
-  # end
-
-  # now
-  # http://localhost:3000/questions/1 because of shallow
-  
   def set_question
     @question = Question.find(params[:id])
   end
