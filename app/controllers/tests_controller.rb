8710eba1c0807e727767dc6ecc315ab5b462420b
Tatiana Morozova <tabobo@mail.ru>
2019-12-29 19:07:43 +0300

    method start

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class TestsController < ApplicationController
-  before_action :get_test, only: %i[show edit update destroy]
+  before_action :get_test, only: %i[show edit update destroy start]
+  before_action :set_user, only: :start
 
   def index
     @tests = Test.all
@@ -39,9 +40,18 @@ class TestsController < ApplicationController
     redirect_to tests_path
   end
 
+  def start
+    @user.tests.push(@test)
+    redirect_to @user.test_passage(@test)
+  end
+
   private
   def get_test
     @test = Test.find(params[:id])
+  end
+
+  def set_user
+    @user = User.first
   end
 
   def test_params
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,10 @@ class User < ApplicationRecord
   has_many :tests, through: :test_passages
   has_many :author_tests, class_name: 'Test', foreign_key: :author_id
 
+  def test_passage(test)
+    test_passages.order(id: :desc).find_by(test_id: test.id)
+  end
+
   scope :tests_by_level, lambda { |level|
     joins(:tests).where(tests: { level: level }).distinct
   }
