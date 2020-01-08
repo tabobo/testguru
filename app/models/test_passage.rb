7ecd0b0b0f7cd158f491699bf0f896d34633d84b
Tatiana Morozova <tabobo@mail.ru>
2020-01-08 18:08:21 +0300

    callback next question

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -6,6 +6,7 @@ class TestPassage < ApplicationRecord
   belongs_to :current_question, class_name: 'Question', optional: true
 
   before_validation :before_validation_set_first_question, on: :create
+  before_update :set_next_question
 
   def completed?
     current_question.nil?
@@ -16,7 +17,6 @@ class TestPassage < ApplicationRecord
       self.correct_questions += 1
     end
 
-    self.current_question = next_question
     save!
   end
 
@@ -39,6 +39,10 @@ class TestPassage < ApplicationRecord
 
   def next_question
     test.questions.order(:id).where('id > ?', current_question.id).first
+  end
+
+  def set_next_question
+    self.current_question = next_question
   end
 
 end
