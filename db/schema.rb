bf86e3b7d53dce64cb89b78e6f2003cb23bb06ef
Tatiana Morozova <tabobo@mail.ru>
2020-01-08 15:11:32 +0300

    default_correct_questions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_28_141246) do
+ActiveRecord::Schema.define(version: 2020_01_07_153857) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 2019_12_28_141246) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "current_question_id"
-    t.integer "correct_questions"
+    t.integer "correct_questions", default: 0
     t.index ["current_question_id"], name: "index_test_passages_on_current_question_id"
     t.index ["test_id"], name: "index_test_passages_on_test_id"
     t.index ["user_id"], name: "index_test_passages_on_user_id"
