648a25399ea8da7f4e88a66e01fabfed1ff9a62d
Tatiana Morozova <tabobo@mail.ru>
2020-01-13 14:49:55 +0300

    change name to email in users

---
+++ db/migrate/20200113114305_change_column_name_in_users.rb
@@ -0,0 +1,5 @@
+class ChangeColumnNameInUsers < ActiveRecord::Migration[5.2]
+  def change
+    rename_column :users, :name, :email
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_12_143322) do
+ActiveRecord::Schema.define(version: 2020_01_13_114305) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,7 +60,7 @@ ActiveRecord::Schema.define(version: 2020_01_12_143322) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
