ffcccb97fba716ab62c7b6cb549ca7de08ef76cb
Tatiana Morozova <tabobo@mail.ru>
2020-01-13 15:05:23 +0300

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   get 'users/new'
   root 'tests#index'
 
+  get :signup, to: 'users#new'
+
+  resources :users, only: :create
+
   resources :tests do
     resources :questions, shallow: true, except: :index do
       resources :answers, shallow: true, except: :index
