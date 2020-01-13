2c5ebd1df2de60bb47e8f6f7489f993e5c9540cc
Tatiana Morozova <tabobo@mail.ru>
2020-01-13 14:09:59 +0300

    add password digest to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+require 'digest/sha1'
 
 class User < ApplicationRecord
   has_many :test_passages
@@ -9,9 +10,19 @@ class User < ApplicationRecord
     test_passages.order(id: :desc).find_by(test_id: test.id)
   end
 
+  def authenticate(password_string)
+    digest(password_string) == self.password_digest ? self : false
+  end
+
   scope :tests_by_level, lambda { |level|
     joins(:tests).where(tests: { level: level }).distinct
   }
 
   validates :name, presence: true
+end
+
+private
+
+def digest(string)
+  Digest::SHA1.hexdigest(string)
 end
