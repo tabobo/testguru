27a02b4d77fc5f383aba6c4eba9ae8e49f493a3b
Tatiana Morozova <tabobo@mail.ru>
2020-01-14 15:07:54 +0300

    validations in user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@
 require 'digest/sha1'
 
 class User < ApplicationRecord
+
+  attr_reader :password
+  attr_writer :password_confirmation
+
   has_many :test_passages
   has_many :tests, through: :test_passages
   has_many :author_tests, class_name: 'Test', foreign_key: :author_id
@@ -18,11 +22,23 @@ class User < ApplicationRecord
     joins(:tests).where(tests: { level: level }).distinct
   }
 
-  validates :name, presence: true
-end
+  validates :email, presence: true
+  validates :password, presence: true, if: Proc.new { |u| u.password_digest.blank? }
+  validates :password, confirmation: true
 
-private
 
-def digest(string)
-  Digest::SHA1.hexdigest(string)
+  def password=(password_string)
+    if password_string.nil?
+      self.password_digest = nil
+    elsif password_string.present?
+      @password = password_string
+      self.password_digest = digest(password_string)
+    end
+  end
+
+  private
+
+  def digest(string)
+    Digest::SHA1.hexdigest(string)
+  end
 end
