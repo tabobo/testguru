8f332fe390d6f9e37944b2ae49dbf7684c3df731
Tatiana Morozova <tabobo@mail.ru>
2020-01-14 15:36:44 +0300

    auth.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,42 +3,20 @@ require 'digest/sha1'
 
 class User < ApplicationRecord
 
-  attr_reader :password
-  attr_writer :password_confirmation
+  include Auth
 
   has_many :test_passages
   has_many :tests, through: :test_passages
   has_many :author_tests, class_name: 'Test', foreign_key: :author_id
 
+  has_secure_password
+
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test_id: test.id)
-  end
-
-  def authenticate(password_string)
-    digest(password_string) == self.password_digest ? self : false
   end
 
   scope :tests_by_level, lambda { |level|
     joins(:tests).where(tests: { level: level }).distinct
   }
 
-  validates :email, presence: true
-  validates :password, presence: true, if: Proc.new { |u| u.password_digest.blank? }
-  validates :password, confirmation: true
-
-
-  def password=(password_string)
-    if password_string.nil?
-      self.password_digest = nil
-    elsif password_string.present?
-      @password = password_string
-      self.password_digest = digest(password_string)
-    end
-  end
-
-  private
-
-  def digest(string)
-    Digest::SHA1.hexdigest(string)
-  end
 end
