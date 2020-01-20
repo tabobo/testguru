52950da747018495f38e6004ae953d420e8abad8
Tatiana Morozova <tabobo@mail.ru>
2020-01-20 14:43:20 +0300

    creating a session

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class TestsController < ApplicationController
+
+  before_action :authenticate_user!
   before_action :get_test, only: %i[show edit update destroy start]
   before_action :set_user, only: :start
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   has_many :tests, through: :test_passages
   has_many :author_tests, class_name: 'Test', foreign_key: :author_id
 
-  has_secure_password
+  #has_secure_password
 
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test_id: test.id)
