3a5da9132ded1e34aae52f78b3518b7ad3cec9a6
Tatiana Morozova <tabobo@mail.ru>
2020-02-28 15:32:16 +0300

    admin panel after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@
 
 class ApplicationController < ActionController::Base
 
+  def after_sign_in_path_for(resource)
+    if current_user.is_a?(Admin)
+      admin_tests_path
+    else
+      root_path
+    end
+  end
 end
