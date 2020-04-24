227ac2147737932def7388608650cf7b7f607a4a
Tatiana Morozova <tabobo@mail.ru>
2020-04-24 14:47:43 +0300

    remove locale from url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,8 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_locale
 
-  def default_url_options
-    { lang: I18n.locale }
+  def default_url_options(options={})
+    (I18n.locale.to_sym.eql?(I18n.default_locale.to_sym) ? {} : {locale: I18n.locale}).merge options
   end
 
   def after_sign_in_path_for(_resource)
@@ -19,7 +19,7 @@ class ApplicationController < ActionController::Base
   private
 
   def set_locale
-    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
+    I18n.locale = params[:locale] || I18n.default_locale
   end
 
   protected
