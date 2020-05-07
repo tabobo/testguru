e14cc48e053050f7ee958a0dee21f2366acb740e
Tatiana Morozova <tabobo@mail.ru>
2020-05-07 18:57:19 +0300

    creating gists

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,8 @@ module TestGuru
     config.load_defaults 5.2
     config.time_zone = 'St. Petersburg'
     config.i18n.default_locale = :ru
+
+    config.autoload_paths << "#{Rails.root}/lib/clients"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
